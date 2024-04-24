12fe79c32bd185a48d797a8e5b0a7105e73abe85
uliapavlova <pavlova.ua.83@gmail.com>
2024-04-24 17:09:49 +0200

    home work_1

---
+++ lesson_3/Home_work_1.py
@@ -0,0 +1,19 @@
+x = int(input("enter number"))
+y = int(input("enter number"))
+print(x + y)
+
+x = int(input("enter number"))
+y = int(input("enter number"))
+print(x * y)
+
+x = int(input("enter number"))
+y = int(input("enter number"))
+print(x / y)
+
+if y is not(y  == 0):
+    print(y)
+
+x = int(input("enter number"))
+y = int(input("enter number"))
+print(y - x)
+
