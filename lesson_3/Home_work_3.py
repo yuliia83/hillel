8e6bc634d694a39012af54351547b3cba67598fb
uliapavlova <pavlova.ua.83@gmail.com>
2024-04-29 17:14:24 +0200

    home work_3

--- lesson_3/Home_work_3.py
+++ lesson_3/Home_work_3.py
@@ -1,7 +1,7 @@
-lst = [1, 2, 3, 4, 5, 6, 7]
-print( [lst[:3], lst [ 3:]])
-x = lst [:3]
-y = lst [3:]
+lst = [1, 2, 3, 4, 5, 6, 7, 9]
+print( [lst[::], lst [::]])
+x = lst [::]
+y = lst [:]
 print(x)
 print(y)
 for num in y:
