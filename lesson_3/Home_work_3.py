de64667a831a4679550954e0259a9efe4aecb93e
uliapavlova <pavlova.ua.83@gmail.com>
2024-05-01 19:27:21 +0200

    home work_3

--- lesson_3/Home_work_3.py
+++ lesson_3/Home_work_3.py
@@ -1,12 +1,9 @@
-lst = [1, 2, 3, 4, 5, 6, 7, 9]
-print( [lst[::], lst [::]])
-x = lst [::]
-y = lst [::]
-print(x)
-print(y)
-for num in y:
-    x.append(num)
-    print(x)
+import math
 
-
-
+lst = [0, 1, 2, 3, 4, 5, 6]
+length = len(lst)
+mid = math.ceil(length / 2)
+left = lst[0:mid]
+right = lst[mid:length]
+result = [left, right]
+print(result)
