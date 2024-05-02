8009407af31cdf3406af4d2208487fea21778801
uliapavlova <pavlova.ua.83@gmail.com>
2024-05-02 16:38:20 +0200

    home work_2

--- lesson_4/home_work_2.py
+++ lesson_4/home_work_2.py
@@ -1,13 +1,12 @@
-lst = [1, 3, 5, 7, 2]
-
-even_sum = 0
-for i in range(0, len(lst), 2):
-    even_sum += lst[i]
+lst = [ 1, 3, 5]
 if not lst:
     result = 0
 else:
     even_sum = sum(lst[:-1])
     last_element = lst[-1]
+even_sum = 0
+for i in range(0, len(lst), 2):
+    even_sum += lst[i]
     result = even_sum * last_element
 print(result)
 
