e4c25cccdb64fbea9eca08478a733ec14bb4ca3b
uliapavlova <pavlova.ua.83@gmail.com>
2024-05-10 13:07:12 +0200

    home work_3

---
+++ lesson_7/Home_work_3.py
@@ -0,0 +1,11 @@
+def sekond_index (string, target):
+    first_index = string.find (target)
+    if first_index == -1:
+        return None
+    second_index = string.find(target, first_index + 1)
+    if second_index == -1:
+        return None
+    return second_index
+
+
+print(sekond_index("variant", "a"))
