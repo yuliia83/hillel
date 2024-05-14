9cd4c4ec1911507fad741453b3c2a791a992d917
uliapavlova <pavlova.ua.83@gmail.com>
2024-05-14 13:29:35 +0200

    home work_1

--- lesson_7/Home_work_1.py
+++ lesson_7/Home_work_1.py
@@ -1,4 +1,8 @@
 def say_hi(name: str, age: int) -> str:
-    return f"Hi, how you, Maks? A you 25 old, it's true?"
+    return f"Hi. My name is {name} and I'm {age} years old"
 
-print(say_hi("Maks", 25))
+if __name__ == "__main__" :
+    assert say_hi("Alex", 32) == ("Hi. My name is Alex and I'm 32 years old")
+    assert say_hi("Frank", 68) == ("Hi. My name is Frank and I'm 68 years old")
+
+    print(say_hi("Maks", 25))
