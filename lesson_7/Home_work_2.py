d3568158e66e2e3cb3b4a9ef0877d9dd51b296df
uliapavlova <pavlova.ua.83@gmail.com>
2024-05-14 13:15:32 +0200

    home work_2

--- lesson_7/Home_work_2.py
+++ lesson_7/Home_work_2.py
@@ -1,10 +1,23 @@
-def correct_sentence(sentence1, sentence2):
-    corrected_sentence1 = sentence1.capitalize()[:-1] + '.' if sentence1[-1] in ['.', '!', '?'] else sentence1.capitalize() + '.'
-    corrected_sentence2 = sentence2.capitalize()[:-1] + '.' if sentence2[-1] in ['.', '!', '?'] else sentence2.capitalize() + '.'
+from typing import Tuple
+
+
+def correct_sentence(sentence1: str, sentence2: str) -> Tuple[str, str]:
+    corrected_sentence1 = sentence1[0].upper() + sentence1[1:-1] + '.' if sentence1[-1] in ['.', '!', '?'] else \
+    sentence1[0].upper() + sentence1[1:] + '.'
+    corrected_sentence2 = sentence2[0].upper() + sentence2[1:-1] + '.' if sentence2[-1] in ['.', '!', '?'] else \
+    sentence2[0].upper() + sentence2[1:] + '.'
     return corrected_sentence1, corrected_sentence2
 
-sentence1 = "hello world!"
-sentence2 = "i love python"
-corrected_sentence1, corrected_sentence2 = correct_sentence(sentence1, sentence2)
-print(corrected_sentence1)
-print(corrected_sentence2)
+
+if __name__ == "__main__":
+    assert correct_sentence("greetings, friends", "Hallo") == ("Greetings, friends.", "Hallo.")
+    assert correct_sentence("Greetings. Friends", "Greetings, friends.") == (
+    "Greetings. Friends.", "Greetings, friends.")
+
+    sentence1: str = "hello world!"
+    sentence2: str = "i love python"
+
+    corrected_sentence1, corrected_sentence2 = correct_sentence(sentence1, sentence2)
+
+    print(corrected_sentence1)
+    print(corrected_sentence2)
